bc705cb6ce211ff0409d4f7b545233843d1514c7
mr-grixa <grixa2403@gmail.com>
2022-12-11 00:52:01 +0300

    Добавьте файлы проекта.

---
+++ L3/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace L3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_load = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem_start = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_finish = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(658, 12);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(75, 23);
+            this.button_load.TabIndex = 1;
+            this.button_load.Text = "Загрузка";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(658, 41);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 2;
+            this.button_save.Text = "Сохранить";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Загрузка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_start,
+            this.toolStripMenuItem_finish});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(116, 48);
+            // 
+            // toolStripMenuItem_start
+            // 
+            this.toolStripMenuItem_start.Name = "toolStripMenuItem_start";
+            this.toolStripMenuItem_start.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem_start.Text = "Старт";
+            this.toolStripMenuItem_start.Click += new System.EventHandler(this.toolStripMenuItem_start_Click);
+            // 
+            // toolStripMenuItem_finish
+            // 
+            this.toolStripMenuItem_finish.Name = "toolStripMenuItem_finish";
+            this.toolStripMenuItem_finish.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem_finish.Text = "Финиш";
+            this.toolStripMenuItem_finish.Click += new System.EventHandler(this.toolStripMenuItem_finish_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.button_load);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Щербинин Григорий 201-325";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_start;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_finish;
+    }
+}
+
---
+++ L3/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace L3
+{
+    public partial class Form1 : Form
+    {
+        string[] map;
+        string[] space=new string[40] ;
+        Point start, finish;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            OpenFileDialog1.FilterIndex = 2;
+            OpenFileDialog1.RestoreDirectory = true;
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sw = new StreamReader(OpenFileDialog1.FileName, false))
+                {
+                    map = sw.ReadToEnd().Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                }
+            }
+            draw();
+
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName, false))
+                {
+                    foreach (string s in map)
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+            }
+        }
+
+        public void draw()
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            for (int y = 0; y < map.Length; y++)
+            {
+                for (int x = 0; x < map[y].Length; x++)
+                {
+                    if (map[y][x] == '#')
+                    {
+                        g.FillRectangle(Brushes.Black, x * 10, y * 10, 10, 10);
+                    }
+                    else
+                    {
+                        g.FillRectangle(Brushes.White, x * 10, y * 10, 10, 10);
+                    }
+                }
+            }
+            map.CopyTo(space,0);
+            for (int y = 1; y < space.Length-1; y++)
+            {
+                for (int x = 1; x < space[y].Length-1; x++)
+                {
+                    if (map[y][x] == '#')
+                    {
+                        space[y] = space[y].Remove(x-1, 3).Insert(x-1, "###");
+                        space[y + 1] = space[y + 1].Remove(x - 1, 3).Insert(x - 1, "###");
+                        space[y - 1] = space[y - 1].Remove(x - 1, 3).Insert(x - 1, "###");
+                    }
+
+                }
+            }
+            for (int y = 2; y < space.Length-2; y++)
+            {
+                for (int x = 2; x < space[y].Length-2; x++)
+                {
+                    if (space[y][x] != '#')
+                    {
+                        g.FillRectangle(Brushes.LightGreen, x * 10, y * 10, 10, 10);
+                    }
+                }
+            }
+            g.FillEllipse(Brushes.Green, start.X * 10, start.Y * 10, 10, 10);
+            g.FillEllipse(Brushes.Blue, finish.X * 10, finish.Y * 10, 10, 10);
+        }
+        int x, y;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (map != null)
+            {
+                x = e.X / 10;
+                y = e.Y / 10;
+                if (e.Button == MouseButtons.Left)
+                {
+                    Graphics g = pictureBox1.CreateGraphics();                  
+                    if (map[y][x] == '#')
+                    {
+                        g.FillRectangle(Brushes.White, x * 10, y * 10, 10, 10);
+                        map[y] = map[y].Remove(x, 1).Insert(x, ".");
+                    }
+                    else
+                    {
+                        g.FillRectangle(Brushes.Black, x * 10, y * 10, 10, 10);
+                        map[y] = map[y].Remove(x, 1).Insert(x, "#");
+                    }
+                    draw();
+                }
+                else
+                {
+                    Point xy = Cursor.Position;
+                    contextMenuStrip1.Show(xy);
+                    
+                }
+
+            }
+
+        }
+
+        private void toolStripMenuItem_finish_Click(object sender, EventArgs e)
+        {
+            finish = new Point(x, y);
+            draw();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw();
+        }
+
+        private void toolStripMenuItem_start_Click(object sender, EventArgs e)
+        {
+            start =new Point(x, y);
+            draw();
+        }
+
+    }
+}
